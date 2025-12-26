f3104978bdd150473e9813584dbeb8a386bd53ca
rviw <216364371+rviw@users.noreply.github.com>
2025-12-27 08:50:53 +0900

    Implement getComputerChoice function for Rock Paper Scissors game

--- projects/rock-paper-scissors/javascript.js
+++ projects/rock-paper-scissors/javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const computerChoice = Math.random() * 3;
+    if (computerChoice < 1) {
+        return "rock";
+    } else if (computerChoice < 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
