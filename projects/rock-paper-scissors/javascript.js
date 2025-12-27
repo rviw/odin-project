06fb2098c8148bd1a53f91c92c482a430ef70b29
rviw <216364371+rviw@users.noreply.github.com>
2025-12-27 09:55:57 +0900

    Implement playRound function to play a single round and scoring

--- projects/rock-paper-scissors/javascript.js
+++ projects/rock-paper-scissors/javascript.js
@@ -17,5 +17,27 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    console.log(`YOU: ${humanChoice}\nCOMPUTER: ${computerChoice}`)
+
+    if (humanChoice === computerChoice) {
+        console.log("Draw");
+    } else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    console.log(`Your score: ${humanScore}\nComputer's score: ${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
