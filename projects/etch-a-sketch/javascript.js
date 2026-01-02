da16bf4b13e609af81d11f3c7b82a4bbf28cee7b
rviw <216364371+rviw@users.noreply.github.com>
2026-01-02 11:09:31 +0900

    Add mouseenter event to change square color in grid

--- projects/etch-a-sketch/javascript.js
+++ projects/etch-a-sketch/javascript.js
@@ -3,5 +3,10 @@ const container = document.querySelector('.grid-container');
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
+
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'blue';
+    });
+
     container.appendChild(square);
 }
