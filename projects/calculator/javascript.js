1a275d98c265cd6598f9b04e3226159f8f9e8704
rviw <216364371+rviw@users.noreply.github.com>
2026-01-05 08:32:03 +0900

    Implement operate function to handle multiple arithmetic operations

--- projects/calculator/javascript.js
+++ projects/calculator/javascript.js
@@ -14,7 +14,24 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(add(1, 3));
-console.log(subtract(5, 2));
-console.log(multiply(4, 2));
-console.log(divide(10, 3));
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case '*':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+let num1 = 1;
+let num2 = 3;
+let operator = '/';
+
+console.log(operate(operator, num1, num2));
