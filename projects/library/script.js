d15899e6cb510c5e07bf8f0a0b75e35eec2caa52
rviw <216364371+rviw@users.noreply.github.com>
2026-01-22 21:07:17 +0900

    Fix whitespace

--- projects/library/script.js
+++ projects/library/script.js
@@ -63,7 +63,7 @@ class LibraryApp {
         });
     }
 
-    addBook( { title, author, pages, isRead }) {
+    addBook({ title, author, pages, isRead }) {
         const book = new Book({
             id: this.createId(),
             title,
@@ -189,7 +189,7 @@ class LibraryApp {
             <path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
           </svg>
         `;
-        
+
         actionsCell.appendChild(removeBtn);
 
         row.append(titleCell, authorCell, pagesCell, statusCell, actionsCell);
